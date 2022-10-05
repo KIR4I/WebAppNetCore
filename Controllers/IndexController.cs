1fb46fc1e75817894da0a39e981698a3a62baed5
Alexander Bogatov <alexanderb@humanforce.com>
2022-10-05 19:53:29 +0300

    Replace razor controllers with MVC

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebAppNetCoreMVC.Models;
+
+namespace WebAppNetCoreMVC.Controllers
+{
+    public class IndexController : Controller
+    {
+        private readonly ILogger<IndexController> _logger;
+
+        public IndexController(ILogger<IndexController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppNetCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
